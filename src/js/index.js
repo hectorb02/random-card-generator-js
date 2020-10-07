bac6bf44b36a5000f8fa66056487c7d0ec2bef93
hectorb02 <hbravo869@gmail.com>
2020-10-07 19:27:46 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,18 @@
 /* eslint-disable */
 
-import "../assets/img/rigo-baby.jpg";
-import "../assets/img/4geeks.ico";
-//import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  var num = ["2", "3", "4", "5", "6", "7 ", "8", "9", "10", "J", "K", "Q", "A"];
+  var suit = ["&#9824", "&#9829", "&#9827", "&#9830"];
+
+  var ranNum = num[Math.floor(Math.random() * num.length)];
+  var ranSuit = suit[Math.floor(Math.random() * suit.length)];
+
+  if (ranSuit == "&#9829" || ranSuit == "&#9830") {
+    document.querySelector(".card").style.color = "red";
+  }
+  document.querySelector(".suit1").innerHTML = ranSuit;
+  document.querySelector(".value").innerHTML = ranNum;
+  document.querySelector(".suit2").innerHTML = ranSuit;
 };
